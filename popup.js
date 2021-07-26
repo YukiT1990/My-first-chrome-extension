d0cf7fb8921ce4aca4859da747b33ad0f981077b
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-07-26 23:18:12 +0900

    my first chrome extension

---
+++ popup.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  const linksList = document.getElementById('linksList');
+  const url = 'https://jqq-utils.netlify.app/api/recentYTVideos';
+
+  const copy = (e) => {
+    const str = e.target.dataset.url;
+    alert(str);
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.removeChild(el);
+  }
+
+  try {
+    const res = await fetch(url);
+    const videos = await res.json();
+    const videosHTML = videos.map(video => {
+      const videoUrl = `https://www.youtube.com/watch?v=${video.videoId}`;
+
+      return `<li class="video-link">
+        <button class="btn" data-url="${videoUrl}">Copy URL</button>
+        <a class="btn" href="${videoUrl}" rel="noopener noreferrer"
+        target="_blank">Watch</a>
+        ${video.title}
+      </li>
+      `
+    }).join('');
+    linksList.innerHTML = videosHTML;
+    const videoLinks = [...document.querySelectorAll('.video-link')];
+    videoLinks.forEach(link => link.addEventListener('click', copy));
+  } catch (err) {
+    console.error(err);
+  }
+});
